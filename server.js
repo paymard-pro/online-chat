85f6827f0643e363aa05ffa297ee63ca07f1ac34
sharifwork <152766157+sharifwork@users.noreply.github.com>
2025-07-12 11:16:54 +0330

    create contacts logic 🤞

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const http = require('http').createServer(app);
 const path = require('path'); // برای کار با مسیر فایل‌ها
 const  { v4 : uuidv4 } = require('uuid');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('chat.db');
+const Database = require('better-sqlite3');
+// باز کردن یا ساخت دیتابیس chat.db
+const db = new Database('chat.db', { verbose: console.log }); // اگه خواستی لاگ کوئری‌ها رو ببینی
+
 app.use(express.json());
 
 // سرو کردن فایل‌های استاتیک (مثل HTML, CSS, JS)
@@ -23,27 +25,26 @@ const io = require('socket.io')(http, {
 });
 
 // لیست کاربران و اتاق‌ها
-const users = [];
-const rooms = {};
+const users = []; // [ ... , {id:.. , name:.. , avatar:.. } ]
+const sockets  = {};
 
-db.run(`
+db.prepare(`
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         userId TEXT UNIQUE,
         name TEXT ,
         avatar INTEGER
     )
-`)
+`).run();
 
-db.run(`
+db.prepare(`
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        content TEXT ,
-        sender_id TEXT,
-        receiver_id TEXT, 
-        seen INTEGER
+        contact TEXT ,
+        send TEXT,
+        receive TEXT
     )
-`)
+`).run();
 
 
 app.post("/api/users", (req, res) => {
@@ -54,49 +55,71 @@ app.post("/api/users", (req, res) => {
 
     const uniqueId = uuidv4();
 
-    db.run('INSERT INTO users (name , userId , avatar) VALUES (?,?,?)' , [name , uniqueId , avatar] , (err) => {
-        if(err){
-            console.log(err);
-            return
-        }
+    db.prepare('INSERT INTO users (name , userId , avatar) VALUES (?,?,?)').run(name, uniqueId, avatar);
+    let newUser = {id: uniqueId , name , avatar };
+    users.push(newUser);
 
-    })
+    console.log(users);
 
     res.status(201).json({id:uniqueId});
 
 });
 
 
+app.get('api/users' , (req , res) => {
+    res.json(users);
+})
+
+app.post('/api/data' , (req , res) => {
+    let data = req.body;
+    const id = data.id;
+    const messagesMe = db.prepare(`SELECT contact , send , receive FROM messages WHERE send = ? OR receive = ?`).all(id , id);
+    const usersMe = db.prepare(`SELECT DISTINCT u.name , u.avatar , u.userId FROM users u WHERE u.userId != ? AND (u.userId IN (SELECT receive FROM messages WHERE send = ?) OR u.userId IN (SELECT send FROM messages WHERE receive = ?))`).all(id , id , id);
+    const me = db.prepare(`SELECT name , userId AS id , avatar FROM users WHERE userid = ?`).all(id);
+    users.push(me);
+    res.json({messages:messagesMe , users:usersMe , me});
+})
+
 io.on('connection', (socket) => {
-    console.log('کاربر متصل شد:', socket.id);
 
-    socket.on('joinChat', (username) => {
-        if(users.length >= 2) {
-            console.log('chat is full!');
-            socket.emit('chatFull');
-            return;
-        }
-        users.push(socket);
-        socket.emit('message', 'به چت خوش آمدید!');
-        console.log(`${username} وارد چت شد.`);
-    });
+    if(socket.userId) {
+        console.log('user connected : ', socket.userId);
+    }
+    else
+        console.log('user connected first time: ', socket.id);
 
-    socket.on('sendMessage', (message) => {
-        console.log(`${users[0].id} - ${users[1].id} - ${users.length}`);
-        socket.emit('messageSend', `${message}`);
-        users.filter(s => s !== socket)[0].emit('messageReceive', `${message}`);
-        console.log(`پیام: ${message}`);
-    });
+    socket.on('setSocket' , (data) => {
+        sockets[data.id] = socket;
+        socket.userId = data.id;
+
+        io.to('contacts').emit('addContact' , users.find(u => u.id == data.id));
+
+    })
+
+    socket.on('joinContacts' , () => {
+        socket.join('contacts');
+    })
+
+    socket.on('leaveContacts', () => {
+        socket.leave('contacts');
+    })
+
+    socket.on('send' , (data) => {
+        db.prepare(`INSERT INTO messages (contact , send , receive) VALUES (? , ? , ?)`).run(data.message , data.send , data.receive);
+        sockets[data.receive].emit('receive' , {message: data.message , send: data.send}) ;
+    })
 
     socket.on('disconnect', () => {
-        console.log('کاربر خارج شد:');
-        delete users[socket];
+        const id = socket.userId;
+
+        console.log('user disconnected : ' , id);
+
+        delete sockets[id];
+        users.splice(users.indexOf(users.find(u => u.id == id)) , 1);
+
+        io.to('contacts').emit('remContact' , {id});
     });
 
-    socket.on('resetData', () => {
-        users.length = 0;
-        console.log('clear all users');
-    });
 });
 
 http.listen(process.env.PORT || 3000, () => {
